75738786d62e05392730c443db68e91aedd78e64
narumiya1 <narumiiyamei@gmail.com>
2023-04-08 21:24:31 +0700

    top banner

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.componentsbar.SearchBar
 import com.example.myapplication.ui.theme.JetzCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +39,24 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun JetCoffeeApps(){
+    Column {
+        Banner()
+    }
 
+}
+@Composable
+fun Banner(
+    modifier: Modifier= Modifier,
+){
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription ="Image Banner",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(165.dp)
+        )
+        SearchBar()
+    }
 }
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
---
+++ app/src/main/java/com/example/myapplication/ui/componentsbar/SearchBar.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication.ui.componentsbar
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+import com.example.myapplication.ui.theme.JetzCoffeeTheme
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = "",
+        onValueChange ={} ,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        //        modifier = Modifier.padding(18.dp),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview(){
+    JetzCoffeeTheme {
+        SearchBar()
+    }
+}
