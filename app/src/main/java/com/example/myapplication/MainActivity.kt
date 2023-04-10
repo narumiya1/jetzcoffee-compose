9d53cfc78c419d1cbe40e49268338d73f84b851b
narumiya1 <narumiiyamei@gmail.com>
2023-04-10 20:16:43 +0700

    main activity update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -20,16 +23,14 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.model.Menu
-import com.example.myapplication.model.dummyBestSellerMenu
-import com.example.myapplication.model.dummyCategory
-import com.example.myapplication.model.dummyMenu
+import com.example.myapplication.model.*
 import com.example.myapplication.ui.componentcategory.CategoryItem
 import com.example.myapplication.ui.componentmenu.MenuItem
 import com.example.myapplication.ui.components.HomeSections
 import com.example.myapplication.ui.components.SectionText
 import com.example.myapplication.ui.componentsbar.SearchBar
 import com.example.myapplication.ui.theme.JetzCoffeeTheme
+import com.example.myapplication.ui.theme.LightGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +53,13 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun JetCoffeeApps(){
-    Column (modifier = Modifier.verticalScroll(rememberScrollState())) {
+    Scaffold(
+        bottomBar = { BottomBar() }
+        ){ innerPadding->
+    Column (
+        modifier = Modifier.verticalScroll(rememberScrollState())
+            .padding(innerPadding)
+    ) {
         Banner()
 //        SectionText(stringResource(R.string.section_category))
 //        CategoryRow()
@@ -74,7 +81,7 @@ fun JetCoffeeApps(){
             MenuRow(dummyBestSellerMenu)
         }
     }
-
+    }
 }
 @Composable
 fun Banner(
@@ -148,5 +155,47 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     JetzCoffeeTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+){
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.primary,
+        modifier=Modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountBox
+            ),
+        )
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                label={
+                    Text(it.title)
+                },
+                selected = it.title==navigationItems[0].title,
+                unselectedContentColor = LightGray,
+                onClick = {  }
+            )
+        }
     }
 }
